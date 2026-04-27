ca0525402d6b1d0b39d1162d03ce874df9667cd7
salt-die <salt-die@protonmail.com>
2026-04-27 13:52:38 -0500

    Enable Masque button skinning support.

--- ElvUI/Init.lua
+++ ElvUI/Init.lua
@@ -187,6 +187,7 @@ end
 local LoadUI = CreateFrame("Frame")
 LoadUI:RegisterEvent("PLAYER_LOGIN")
 LoadUI:SetScript("OnEvent", function()
+	AddOn.Masque = AddOn.Masque or LibStub("Masque", true)
 	AddOn:Initialize()
 end)
 
